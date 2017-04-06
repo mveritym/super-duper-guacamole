2ebff22fe7f221839a2e51c307c787b4ef445d34
mveritym <mveritym@gmail.com>
2017-04-06 16:48:31 +0100

    Removing boilerplate, old template

---
+++ src/components/Main.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <div class="main">
+    <h2>Melissa Marshall</h2>
+    <navigation></navigation>
+  </div>
+</template>
+
+<script>
+import Navigation from './Navigation';
+
+export default {
+  name: 'main',
+  components: {
+    Navigation,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="navigation">
+    <ul>
+      <li>projects</li>
+      <li>blog</li>
+      <li>github</li>
+      <li>twitter</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'navigation',
+};
+</script>
+
+<style scoped>
+
+  li {
+    list-style: none;
+  }
+
+</style>
