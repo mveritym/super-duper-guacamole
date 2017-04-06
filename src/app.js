ff0b97b9bacf9118ac634730b8ab90f34bb9fddc
mveritym <mveritym@gmail.com>
2017-04-06 15:15:41 +0100

    Basic server

---
+++ src/app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.use('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(3000, () => {
+  console.log('Website running on port 3000');
+});
