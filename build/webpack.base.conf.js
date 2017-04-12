af8127f734770ec6c20172b3e3fe3bc990fe8ef2
Melissa Marshall <mveritym@gmail.com>
2017-04-13 00:22:38 +0100

    put it all back

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -4,12 +4,12 @@ var config = require('../config')
 var vueLoaderConfig = require('./vue-loader.conf')
 
 function resolve (dir) {
-  return path.join('/home/mel/website', dir)
+  return path.join(__dirname, '..', dir)
 }
 
 module.exports = {
   entry: {
-    app: '/home/mel/website/src/main.js'
+    app: './src/main.js'
   },
   output: {
     path: config.build.assetsRoot,
--- config/index.js
+++ config/index.js
@@ -4,8 +4,8 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: '/home/mel/website/dist/public/index.html',
-    assetsRoot: '/home/mel/website/dist',
+    index: path.resolve(__dirname, '../dist/index.html'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     productionSourceMap: true,
