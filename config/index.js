f0afe9de5cebe705e5cf9cfb1587d787473006fd
Melissa Marshall <mveritym@gmail.com>
2017-04-13 00:13:00 +0100

    forgot a comma

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve('/home/mel/website' 'dist/public/index.html'),
+    index: path.resolve('/home/mel/website', 'dist/public/index.html'),
     assetsRoot: path.resolve('/home/mel/website', '../dzist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
