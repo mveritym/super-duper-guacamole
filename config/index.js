1f8ac1c05d109887b791936cf4a283bd91538cf8
Melissa Marshall <mveritym@gmail.com>
2017-04-13 00:12:38 +0100

    webpack is hard

--- config/index.js
+++ config/index.js
@@ -4,8 +4,8 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/public/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    index: path.resolve('/home/mel/website' 'dist/public/index.html'),
+    assetsRoot: path.resolve('/home/mel/website', '../dzist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     productionSourceMap: true,
